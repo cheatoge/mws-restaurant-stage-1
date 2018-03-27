ac67a4e31f6c718c5ce0d4fa3623f67a450ead10
Łukasz Szpil <cheatoge@gmail.com>
2018-03-27 16:11:21 +0200

    Service worker: pages are now available offline (without images)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', {scope: "/"}).then( reg => {
-    console.log("sw registered");
-  }).catch( error => {
-    console.log("did not register sw,", error);
-  });
+  navigator.serviceWorker.register('sw.js', {scope: "/"}).then(function(registration) {
+    let serviceWorker;
+    if (registration.installing) {
+      serviceWorker = registration.installing;
+    } else if (registration.waiting) {
+      serviceWorker = registration.waiting;
+    } else if (registration.active) {
+      serviceWorker = registration.active;
+    }
+
+    if (serviceWorker) {
+      console.log("ServiceWorker phase:", serviceWorker.state);
+
+      serviceWorker.addEventListener('statechange', function (e) {
+        console.log("ServiceWorker phase:", e.target.state);
+      });
+    }
+  })
+    .catch(function(err) {
+      console.log('Service Worker Error', err);
+    });
 }
--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const staticCacheName = 'restaurant-reviews-static-v2';
+const staticCacheName = 'restaurant-reviews-static-v4';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(staticCacheName)
       .then( cache => {
       return cache.addAll([
-        "/",
+        '/',
+        '/restaurant.html',
         'data/restaurants.json',
         'js/main.js',
         'js/index.js',
@@ -17,6 +18,20 @@ self.addEventListener('install', function (event) {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('restaurant-reviews-') && cacheName !== staticCacheName;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      )
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then( response => {
@@ -24,3 +39,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+
+
+console.log("4");
