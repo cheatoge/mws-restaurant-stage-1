00ca5aeaeec9908b7944f86f65c90db7ec7c3272
Łukasz Szpil <cheatoge@gmail.com>
2018-03-31 19:59:15 +0200

    Fixed: DBHelper.imageUrlForRestaurant is not a function

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -147,6 +147,13 @@ class DBHelper {
   }
 
   /**
+   * Restaurant image URL.
+   */
+  static imageUrlForRestaurant(restaurant) {
+    return (`/img/${restaurant.photograph}`);
+  }
+
+  /**
    * Map marker for a restaurant.
    */
   static mapMarkerForRestaurant(restaurant, map) {
