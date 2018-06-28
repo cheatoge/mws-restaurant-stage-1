300d6c8bb18df0cdf8326381b579b2432c3f5335
Pan Uszak <uszak777@gmail.com>
2018-06-28 21:14:21 +0200

    Change aria-label to be more descriptive

--- js/main.js
+++ js/main.js
@@ -186,7 +186,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
-  more.setAttribute('aria-label', `view details of ${restaurant.name}`);
+  more.setAttribute('aria-label', `view details of ${restaurant.name} restaurant`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more);
 
